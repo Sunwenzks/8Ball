fa87afe2f2539519e0ada4f192b8fe6a38799f82
Ksyusha <sun_wenz03@mail.ru>
2022-10-22 21:31:26 +0300

    Добавьте файлы проекта.

---
+++ 8Ball/AnswerGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8Ball
+{
+    internal static class AnswerGenerator
+    {
+        private static List<string> answers = new List<string>() { "да", "нет","спросите позже", "очень сомнительно", "незнаю" };
+ 
+
+       public static string GetRandomAnswer()
+       {
+            Random random = new Random();
+            var index = random.Next(answers.Count - 1);
+            return answers[index];
+       }
+    }
+}
---
+++ 8Ball/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _8Ball
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtQuestion_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Answer_Click(object sender, RoutedEventArgs e)
+        {
+            this.Cursor = Cursors.Wait;
+            // Делаем задержку, для создания эффекта того, что программа задумалась.
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            // Берем случайный ответ.
+            txtAnswer.Text = AnswerGenerator.GetRandomAnswer();
+            // Восстанавливаем прежнее состояние курсора.
+            this.Cursor = null;
+
+        }
+    }
+}
